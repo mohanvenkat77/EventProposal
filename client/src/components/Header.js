ebeb32f8a3b6c73aadf0d6122f3e56351ada9ee2
sabeerahamed0511 <sabeerahamed0511@gmail.com>
2023-04-18 12:41:45 +0530

    header component added

---
+++ client/src/components/Header.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+export function Header() {
+    return <>
+    <div className="main-container">
+        <header>
+            <h1 className="logo">LOGO</h1>
+            <nav>
+                <p>Vendor Name</p>
+                <div className="img-container">
+                    <img src="https://thumbs.dreamstime.com/b/default-profile-picture-avatar-photo-placeholder-vector-illustration-default-profile-picture-avatar-photo-placeholder-vector-189495158.jpg" alt="dp" />
+                </div>
+            </nav>
+        </header>
+        <div className="outlet-container">
+
+        </div>
+    </div>
+    </>
+}

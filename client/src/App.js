f87b6852936a7a416c053e4bc4e9d6785bebf49e
Javed Akhtar <javed09523@gmail.com>
2023-04-19 13:47:05 +0530

    vendor and user for frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,28 @@
 import React from 'react'
-import "./styles/app.css";
-import "./styles/header.css";
-import { Header } from './components/Header'
+// import "./styles/app.css";
+// import "./styles/header.css";
+// import "./styles/VendorSignIn.css";
+import VendorSignIn from './components/VendorSign';
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+// import { Header } from './components/Header'
+// import VendorSignIn from './components/VendorSign';
 
 const App = () => {
-  return <>
-  <Header />
-  </>
+//   return <>
+//   {/* <Header /> */}
+//   </>
+// }
+
+// export default App
+return (
+  <div>
+    <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<VendorSignIn/>}/>
+      </Routes>
+    </BrowserRouter>
+  </div>
+  );
 }
 
-export default App
+export default App;
